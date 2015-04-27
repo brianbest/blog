2b483685b8e86361d71951c92f99ef2ac7b2cb61
Brian best <brian.brianb64@gmail.com>
2015-04-27 12:02:05 -0300

    converting html to php

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <title>Life Is Strange | Brian Best Web Dev &amp; Founder</title>
+
+		<script src="//use.typekit.net/jgt7sgm.js"></script>
+		<script>try{Typekit.load();}catch(e){}</script>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		
+		<link rel="stylesheet" href="css/glyphicons-social.css">
+		<link rel="stylesheet" href="css/main.css">
+		<?php 
+			wp_head();
+		?>
+	</head>
+	<body>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-5">
+				<div class="headInfo">
+					<h1>Brian Best</h1>
+					<h3>developer &amp; founder</h3>
+					<div class="socialMedia">
+						<a href="https://ca.linkedin.com/in/brianbest101">
+							<div class="socialItem social social-linked-in"></div>
+						</a>
+						<a href="https://github.com/brianbest">
+							<div class="socialItem social social-github"></div>
+						</a>	
+						<a href="https://twitter.com/BrianBest">
+							<div class="socialItem social social-twitter"></div>
+						</a>
+						<a href="https://www.facebook.com/brianbest101">
+							<div class="socialItem social social-facebook"></div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 blogList">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<a href="<?php echo get_permalink(); ?>">
+					<article>
+						<h1 class="title"><?php the_title(); ?><span><?php the_time('F jS, Y'); ?></span></h1>
+						<p><?php the_excerpt(); ?></p>
+					</article>
+				</a>
+				<?php endwhile; else: ?>
+				<article>
+					<h1><?php _e('Sorry, no posts matched your criteria.'); ?></h1>
+				</article>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>
