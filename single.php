9b2f55bbeefef30c94bce1d100de50a3233c1ee9
Brian best <brian.brianb64@gmail.com>
2015-04-27 17:47:54 -0300

    mail chimp intigrated

--- single.php
+++ single.php
@@ -15,7 +15,7 @@
 		  </div><!-- /.container-fluid -->
 		</nav>
 		<div class="row">
-			<div class="col-md-offset-4 col-md-4 blog">
+			<div class="col-sm-12 col-md-offset-3 col-md-6 col-lg-offset-4 col-lg-4 blog">
 				<article>
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<h6><?php the_time('F jS, Y'); ?></h6>
@@ -58,10 +58,25 @@
 		</div>
 		<footer>
 
-			<form>
-				<input type="text" placeholder="Your Email">
-				<input type="submit" class="btn" value="Get updates on my work">
-			</form>
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup">
+				<form action="//brian-best.us10.list-manage.com/subscribe/post?u=8a568116221771426d6a1e472&amp;id=727a6a8d2c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				    <div id="mc_embed_signup_scroll">
+					
+				<div class="mc-field-group">
+					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Your Email">
+				</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;"><input type="text" name="b_8a568116221771426d6a1e472_727a6a8d2c" tabindex="-1" value=""></div>
+				    <div class="clear"><input type="submit" value="Get updates on my work" name="subscribe" id="mc-embedded-subscribe" class="btn button"></div>
+				    </div>
+				</form>
+			</div>
+
+			<!--End mc_embed_signup-->
 
 		</footer>
 </div>
