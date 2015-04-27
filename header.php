001f575e0b7f235f90cb7a78ab3383ef67de691a
Brian best <brian.brianb64@gmail.com>
2015-04-27 12:07:14 -0300

    ready for uploading to WP

--- header.php
+++ header.php
@@ -7,15 +7,15 @@
 		<meta charset="utf-8">
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <title>Life Is Strange | Brian Best Web Dev &amp; Founder</title>
+	    <title></title>
 
 		<script src="//use.typekit.net/jgt7sgm.js"></script>
 		<script>try{Typekit.load();}catch(e){}</script>
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 		
-		<link rel="stylesheet" href="css/glyphicons-social.css">
-		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/css/glyphicons-social.css">
+		<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/css/main.css">
 		<?php 
 			wp_head();
 		?>
